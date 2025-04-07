67ebec2ff81703731da5a7c3cc9aa8d8022fa2e9
Khush-A <128749586+Khush-A@users.noreply.github.com>
2025-04-07 22:13:24 +0100

    Add files via upload

---
+++ pytorch nn MNISTNet.py
@@ -0,0 +1,49 @@
+# PyTorch Neural Network practice using MNISTNet
+
+
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,))  # Normalizing using MNIST mean and std
+])
+
+train_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+
+
+
+class MNISTNet(nn.Module):
+    def __init__(self):
+        super(MNISTNet, self).__init__()
+        self.fc1 = nn.Linear(28*28, 256)
+        self.bn1 = nn.BatchNorm1d(256)
+        self.dropout = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(256, 10)
+        
+        # Weight initialization example
+        nn.init.kaiming_normal_(self.fc1.weight)
+        nn.init.kaiming_normal_(self.fc2.weight)
+
+    def forward(self, x):
+        x = x.view(-1, 28*28)  # Flatten the image
+        x = F.relu(self.bn1(self.fc1(x)))
+        x = self.dropout(x)
+        x = self.fc2(x)
+        return x
+
+# 4. Instantiate Model and Test on One Batch
+model = MNISTNet()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
+
+
+# Get one batch from the DataLoader
+for images, labels in train_loader:
+    output = model(images)  # Forward pass
+    print(output.shape)     # Should be: torch.Size([64, 10])
+    break
